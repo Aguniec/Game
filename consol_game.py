1deac94f34d3e37d277d7d425eac611afcd0ac1b
Aguniec <aguniecc@gmail.com>
2019-05-14 08:11:23 +0200

    new_package

--- consol_game.py
+++ consol_game.py
@@ -1,3 +1,4 @@
+from creatures.creatures import Creature, Goblin, Human
 
 class Printed:
     def get_name():
@@ -19,70 +20,6 @@ class Printed:
             print (verb(noun_word))
         else:
             print(verb("nothing"))
-
-
-class Creature:
-    class_name = ""
-    descryption = ""
-    objects = {}
-
-    def __init__(self, name):
-        self.name = name
-        Creature.objects[self.class_name] = self
-    
-    def get_descryption(self):
-        return self.class_name + "\n" + self.descryption
-
-
-class Goblin (Creature):
-    def __init__(self, name):
-        self.class_name = "goblin"
-        self._descryption = "A big, green creature"
-        self.health = 3
-        super().__init__(name)
-    
-    @property
-    def descryption(self):
-        if self.health >=3:
-            return self._descryption
-        elif self.health == 2:
-            healh_status = "It's arm has been cut off"
-        elif self.health == 1:
-            healh_status = "It lost its leg"
-        elif self.health <= 0:
-            healh_status = "It's dead"
-        return self._descryption + "\n" + healh_status
-    
-    @descryption.setter
-    def descryption (self, value):
-        self._descryption = value
-
-
-class Human(Creature):
-    def __init__(self, name):
-        self.class_name = "human"
-        self._descryption = "Regular human"
-        self.health = 10
-        super().__init__(name)
-
-    @property
-    def descryption (self):
-        if self.health >=10:
-            return self._descryption
-        elif self.health == 6:
-            healh_status = "It's arm has been cut off"
-        elif self.health == 4:
-            healh_status = "It lost its leg"
-        elif self.heath == 2:
-            healh_status = "It los its both legs"
-        elif self.health <= 0:
-            healh_status = "It's dead"
-        return self._descryption + "\n" + healh_status
-
-    @descryption.setter
-    def descryption (self, value):
-        self._descryption = value
-
 
 class Interactions: 
     def hit(noun):
