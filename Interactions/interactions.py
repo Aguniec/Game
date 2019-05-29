cc4fecb6a190522cf331956ac77114e1e50dd983
Aguniec <aguniecc@gmail.com>
2019-05-29 15:32:52 +0200

    commit

--- Interactions/interactions.py
+++ Interactions/interactions.py
@@ -1,27 +1,8 @@
-from creatures.creatures import Creature, Goblin, Human
+from Creatures.creatures import Creature, Goblin, Human
 
-class Printed:
-    def get_name():
-        print("Hello! What's your name?")
-        name = input("")
-        print("OK {},let's play a game".format(name))
-        
-    def get_input():
-        command = input(":").split()
-        verb_word = command[0]
-        if verb_word in verb_dict:
-            verb = verb_dict[verb_word]
-        else:
-            print("Unknown command {}".format(verb_word))
-            return
-        
-        if len(command) >=2:
-            noun_word = command[1]
-            print (verb(noun_word))
-        else:
-            print(verb("nothing"))
 
-class Interactions: 
+
+class MainInteractions:
     def hit(noun):
         if noun in Creature.objects:
             thing = Creature.objects[noun]
@@ -44,32 +25,16 @@ class Interactions:
     def say(noun):
         return "You said {}".format(noun)
 
+"""
     def ask_name(noun):
         if noun in Creature.objects:
             asked_creature = Creature.objects[noun]
             if asked_creature.health > 0:
-                print("{}: What's your name?".format(Printed.get_name)) #Doesn't work properly
+                print("{}: What's your name?".format(UserInterface.get_name)) #Doesn't work properly
                 msg = "My name is " + asked_creature.name
             else :
                 msg = "Why are you try to interact the dead creature?"
-        else : 
+        else :
             msg = "There is no {} here".format(noun)
         return msg
-
-
-verb_dict = {
-    "say" : Interactions.say,
-    "examine" : Interactions.examine,
-    "hit" : Interactions.hit,
-    "ask_name" : Interactions.ask_name
-}
-
-
-goblin = Goblin("Gooby")
-human = Human("Steve")
-
-
-Printed.get_name()
-
-while True:
-    Printed.get_input()
+"""
