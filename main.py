f6d69fd79fc33ea79edc8b75461784b8f48fee56
Aguniec <aguniecc@gmail.com>
2019-05-30 11:09:05 +0200

    commit

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 from Interactions.interactions import MainInteractions
 from Creatures.creatures import Goblin, Human
 
+
 class UserInterface:
 
+
+    verb_dict = {
+        "say": MainInteractions.say,
+        "examine": MainInteractions.examine,
+        "hit": MainInteractions.hit
+    }
+
+    @staticmethod
     def get_name():
         print("Hello! What's your name?")
         name = input("")
         print("OK {},let's play a game".format(name))
-        
-    def get_interaction():
-        command = input("Select interaction:")
-        return command
 
-    def get_creature():
-        selected_creature = input("Select creature:")
-        return selected_creature
-
+    @staticmethod
     def select_interaction():
-        command = UserInterface.get_interaction()
-        selected_creature = UserInterface.get_creature()
-        if command in verb_dict:
-            selected_interaction = verb_dict[command]
+
+        command = input("Select interaction:")
+        selected_creature = input("Select creature:")
+
+        if command in UserInterface.verb_dict:
+            selected_interaction = UserInterface.verb_dict[command]
             print(selected_interaction(selected_creature))
         else:
             print("Unknown command {}".format(command))
         return selected_interaction
 
 
-verb_dict = {
-    "say" : MainInteractions.say,
-    "examine" : MainInteractions.examine,
-    "hit" : MainInteractions.hit
-}
-
 
 goblin = Goblin("Gooby")
 human = Human("Steve")
 
 
-UserInterface.get_name()
+#UserInterface.get_name()
 
 while True:
 
